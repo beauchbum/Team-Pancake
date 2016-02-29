d49f6117fa791a47a61c17758b3840b54a83f61d
dingy63 <conanjennings@dhcp-10-105-130-247.wireless.northwestern.private>
2016-02-29 13:34:09 -0600

    test

--- recycling.js
+++ recycling.js
@@ -1,10 +1,17 @@
+<<<<<<< Updated upstream
 function drawChart() {
+=======
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawWeeks);
+
+      function drawWeeks() {
+>>>>>>> Stashed changes
         var data = google.visualization.arrayToDataTable([
           ['Week', 'Thrown Away', 'Recycled'],
-          ['1',  1000,      400],
-          ['2',  1170,      460],
-          ['3',  660,       1120],
-          ['4',  1030,      540]
+          ['1',  15,     10],
+          ['2',  11,     16],
+          ['3',  13,     14,
+          ['4',  12,     15]
         ]);
 
         var options = {
